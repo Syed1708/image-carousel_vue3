5ab59bf247866cec76cd05d73abbc4043a509a90
Syed1708 <137244394+Syed1708@users.noreply.github.com>
2023-07-23 15:34:37 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<script setup>
+
+
+import {ref, reactive, onMounted, onUnmounted} from 'vue'
+
+
+
+const carouselItems = [
+  {
+    image: 'https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60',
+    title: 'First Title',
+    caption: 'Fisrt Description'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60',
+    title: 'Second Title',
+    caption: 'Second description'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60',
+    title: 'Third Title',
+    caption: 'Thired Description'
+  },
+  {
+    image: 'https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60',
+    title: 'Fourth Title',
+    caption: 'Fourth Description'
+  },
+
+]
+
+
+const activeIndex = ref(0)
+
+const incrementActiveIndex = () =>{
+  if(activeIndex.value == carouselItems.length -1){
+    activeIndex.value = 0
+  }
+  else{
+    activeIndex.value++
+  }
+}
+
+let timerId = null;
+
+const startSlideShow = function(){
+  timerId = setInterval(incrementActiveIndex, 1000)
+}
+
+
+const stopSlideShow = function(){
+    clearInterval(timerId)
+  }
+ 
+  onMounted(() => {
+    startSlideShow()
+  })
+
+  onUnmounted(() =>{
+    stopSlideShow()
+  })
+
+
+
+</script>
+
+<template>
+
+
+<div >  
+
+  <div @mouseover="stopSlideShow" @mouseleave="startSlideShow" id="carouselExampleCaptions" class="carousel">    
+    
+
+    
+    <div class="carousel-inner">     
+
+      <div class="carousel-item " v-for="(carouselitem, index) in carouselItems" :key="index" :class="index == activeIndex?'active':''">           
+        <img height="500" :src="carouselitem.image" class="d-block w-100 img img-responsive" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>{{ carouselitem.title }}</h5>
+          <p>{{ carouselitem.caption }}</p>
+        </div>
+      </div>  
+
+    </div>    
+    <a @click.prevent="0 == activeIndex ? activeIndex = (carouselItems.length) - 1 : activeIndex--" class="carousel-control-prev" href="#" role="button">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a @click.prevent="(carouselItems.length) - 1 == activeIndex ? activeIndex = 0 : activeIndex++" class="carousel-control-next" href="#" role="button">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+
+
+    </div>
+
+</div>
+
+
+
+
+
+</template>
+
+<style scoped>
+ 
+
+
+</style>
